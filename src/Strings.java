d535d4b1123afc9b535357db614a2138dcba19dd
BT_4N2_34 <ssakhawat4381@bths.edu>
2022-10-06 12:33:10 -0400

    Initial commit

---
+++ src/Strings.java
@@ -0,0 +1,16 @@
+public class Strings {
+    public static void main(String[] args) {
+        String fire = "CRACKLE!";
+        String witch = "CACKLE!";
+        String str5 = fire.substring(2);
+        System.out.println(str5);
+        String str6 = witch.substring(witch.length() - 4);
+        System.out.println(str6);
+        int loc = str5.indexOf(str6);
+        System.out.println(loc);
+        String str7 = fire.substring(loc, loc + 3);
+        System.out.println("the hidden word is: " + str7);
+
+
+    }
+}
